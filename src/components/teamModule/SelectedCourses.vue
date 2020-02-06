2cb672bc5f34cd93452675350bc424b5cc291577
junlee1998 <malijun9077@qq.com>
2020-02-06 22:39:18 +0800

    请求学生已选择课程

---
+++ src/components/teamModule/SelectedCourses.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div id="container" >
+      <template v-for="course in courseList">
+        <el-card class="box-card" shadow="hover">
+          <div slot="header" class="clearfix">
+            <span><b>{{course.course.course_name}}</b></span>
+          </div>
+          <div class="text item">
+            {{'课程编号: ' + course.course.course_code }}
+          </div>
+
+          <div class="text item">
+            {{'学分: ' + course.course.credit }}
+          </div>
+
+          <div class="text item">
+            {{'学时: ' + course.course.hours }}
+          </div>
+
+          <div class="text item">
+            是否组队: {{ course.course.is_team?"是":"否" }}
+          </div>
+
+          <div class="text item" v-if="course.course.is_team">
+            组队最大人数: {{ course.course.max_num }}
+          </div>
+
+
+        </el-card>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "SelectedCourses",
+    data() {
+      return {
+        courseList: JSON.parse(localStorage.userInfo).userCourses
+      }
+    },
+    mounted() {
+      console.log(this.courseList)
+    }
+  }
+</script>
+
+<style scoped>
+  .text {
+    font-size: 14px;
+    border-bottom: 1px solid #d6eef1;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 300px;
+    margin-right: 16px;
+    margin-bottom: 15px;
+
+  }
+
+  #container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin: auto;
+  }
+</style>
